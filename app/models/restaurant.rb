076cee7f5216bf0df816a28a84dcd8cd131b1fb5
Dean Webb <Dean.webb@careerbuilder.com>
2014-12-10 19:15:15 -0500

    Validating values from the form

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,9 @@
 class Restaurant < ActiveRecord::Base
+	validates :name, presence: true
+	validates :cuisine, presence: true
+	validates :address, presence: true
+	validates :cost, presence: true,
+					 length: { minimum: 1, maximum: 5 }
+	validates :rating, presence: true,
+					   length: { minimum: 1, maximum: 5 }
 end
